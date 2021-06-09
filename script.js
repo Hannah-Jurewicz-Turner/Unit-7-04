96eadc59de3654363518dd820d5fdee598b038ab
HannahJT <84517064+Hannah-Jurewicz-Turner@users.noreply.github.com>
2021-06-09 14:41:03 +0000

    Fixed linter errors.

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ let userAge = 11
 let userDate = 'Friday'
 
 function checkAdmission () {
- // Lets program read age as an integer and connects age and date to assigned input boxes.
+  // Lets program read age as an integer and connects age and date to assigned input boxes.
   userAge = document.getElementById('input-one').value
   userAge = parseInt(userAge)
 
   userDate = document.getElementById('input-two').value
 
   // Checks and compares user age and day of the week to the student discount dates and ages.
-  if ((userDate === 'Tuesday' || userDate === 'Thursday')||(userAge > 12 && userAge < 21)){ 
+  if ((userDate === 'Tuesday' | | userDate === 'Thursday')| |(userAge > 12 && userAge < 21)) {
     document.getElementById('answer').innerHTML = 'You get student pricing.'
   } else {
     document.getElementById('answer').innerHTML = 'You get regular pricing.'
